7f46305206e94eb606273d659d6a848af5443db9
parthapaul2023 <tansquire@gmail.com>
2023-05-07 19:20:53 +0530

    Frontend to backend data flow

--- Basic/views.py
+++ Basic/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 def home(request):
     return HttpResponse("This is home page")
@@ -13,4 +13,14 @@ def bootstrap(request):
 def register(request):
     a=5
     b='example'
+
+    if (request.method=='POST'):
+        email1=request.POST['email']
+        password1=request.POST['password']
+        print("Got data from register page: ",email1,password1)
+        if(password1=='123456'):
+            pass
+        else:
+           return redirect('/register') 
+        return redirect('/about')
     return render(request,'register.html', {'test':a, 'test1':b})     
